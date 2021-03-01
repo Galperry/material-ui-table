47372224c6ed7ba2bd1cf33f507d220221e2457a
Gal Perry <galperry2@gmail.com>
2021-03-01 15:00:37 +0200

    finished project

---
+++ src/MyTable.js
@@ -0,0 +1,134 @@
+import React, {useState, useEffect, useRef} from 'react';
+import { DataGrid } from '@material-ui/data-grid';
+import {db} from './firebase'
+
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'fullName', headerName: 'Full Name', width: 130 },
+  { field: 'subject', headerName: 'Subject', width: 130 },
+  { field: 'roll', headerName: 'Role', width: 130,},
+  { field: 'class', headerName: 'Class', width: 160},
+];
+
+export default function MyTable() {
+    const [checked, setChecked] = useState([])
+    const [showAdd, setShowAdd] = useState("none")
+    const [showUpdate, setShowUpdate] = useState("none")
+    const [myData, setMyData] = useState("")
+    const nameRef = useRef(null)
+    const subjectRef = useRef(null)
+    const roleRef = useRef(null)
+    const classRef = useRef(null)
+
+    useEffect(()=> {updateDatabase()},[])
+
+    function updateDatabase(){
+        db.on("value", (snapshot) =>{
+            let myData = ""
+            myData = (snapshot.val().students);
+            setMyData(myData)
+        })
+    }
+
+    function updateItem(e){
+        e.preventDefault()
+        const studentsRef = db.child("students");
+        for (let id of checked){
+            studentsRef.child(id-1).update({
+                "fullName": nameRef.current.value || myData[id-1].fullName,
+                "subject": subjectRef.current.value || myData[id-1].subject,
+                "roll": roleRef.current.value || myData[id-1].roll,
+                "class": classRef.current.value || myData[id-1].class
+            })
+        }
+        nameRef.current.value=""
+        subjectRef.current.value=""
+        roleRef.current.value=""
+        classRef.current.value=""
+        setShowUpdate("none")
+        setChecked([])
+      }
+
+    function removeItem(){
+        if (window.confirm("please confirm to delete item no."+checked.join(","))){
+            console.log(checked[0]-1)
+            for (let id of checked){
+                db.child("students").child(id-1).remove()
+            }
+            setChecked([])
+        }
+    }
+
+    function addData(e){
+        e.preventDefault()
+        var studentsRef = db.child("students");
+        studentsRef.child(myData.length).set({
+          "id":myData.length+1,
+          "fullName": nameRef.current.value,
+          "subject": subjectRef.current.value,
+          "roll": roleRef.current.value,
+          "class": classRef.current.value
+        }).then(()=>setShowAdd("none"));
+      }
+
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+        {myData? 
+        <DataGrid rows={myData.filter((element)=>{return (element!==false)})} columns={columns} pageSize={6} checkboxSelection onSelectionModelChange={(newSelection) => {
+            setChecked(newSelection.selectionModel)
+          }} selectionModel={checked} />
+        :""}
+        <button onClick={removeItem}>remove checked items</button>
+
+        <button onClick={()=>setShowAdd("block")}>Create a new item</button>
+                <form style={{padding:"5px",display:showAdd}} onSubmit={(e)=>addData(e)}>
+                  <div>
+                    <input style={{width:"200px"}} ref={nameRef} placeholder="name" type="text" required/>
+                  </div>
+                  <div>
+                  <span>please select a subject</span><br/>
+                    <select style={{width:"200px"}} ref={subjectRef}>
+                      <option value="Firebase RealTime Database">Firebase RealTime Database</option>
+                      <option value="Math">React</option>
+                    </select>
+                  </div>
+                  <div>
+                    <span>please select a role</span><br/>
+                    <select style={{width:"200px"}} ref={roleRef}>
+                      <option value="Teacher">Teacher</option>
+                      <option value="Student">Student</option>
+                    </select>
+                  </div>
+                  <div>
+                    <span>please select a class</span><br/>
+                    <select style={{width:"200px"}} ref={classRef}>
+                      <option value="Computer Science">Computer Science</option>
+                      <option value="Math">Math</option>
+                    </select>
+                  </div>
+                  <button>ADD DATA</button>
+                </form>
+                <br/>
+                <button onClick={()=>setShowUpdate("block")}>UPDATE DATA</button>
+
+                <form style={{padding:"5px", display:showUpdate}} onSubmit={(e)=>updateItem(e)}>
+                <span>Please leave blank to keep unchanged</span><br/>
+                  <div>
+                    <input ref={nameRef} placeholder="name" type="text" />
+                  </div>
+                  <div>
+                    <input ref={subjectRef} placeholder="subject" type="text" />
+                  </div>
+                  <div>
+                    <input ref={roleRef} type="text" placeholder="role" />
+                  </div>
+                  <div>
+                    <input ref={classRef} type="text" placeholder="class" />
+                  </div>
+                  <button>UPDATE ITEM</button>
+                </form>
+
+    </div>
+  );
+}
